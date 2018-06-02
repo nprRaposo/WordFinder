7bab10f36b1ce817995c429773689e00f910b503
nprRaposo <nikoraposo@gmail.com>
2018-06-02 18:29:45 -0300

    Test

---
+++ WordFinderExercise.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WordFinderExercise.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WordFinderExercise.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49e543ff-d5f3-42ba-8db4-bf4b343e7bdb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WordFinderExercise.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordFinderExercise.Test
+{
+    [TestClass]
+    public class WordFinderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var dictionary = new string[] { "chill", "wind", "snow", "cold" };
+            var src = new string[] { "abcdc", "fgwio", "chill", "pqnsd", "uvdxy" };
+
+            var wordFinder = new WordFinder(dictionary);
+            var result = wordFinder.Find(src);
+
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
